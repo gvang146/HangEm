6cec68c78a7ff6f160728ee9310c6d2e9effdb5e
gvang146 <gvang146@gmail.com>
2021-09-15 16:20:09 -0500

    Update Hangman.js

--- hangem/src/components/Hangman.js
+++ hangem/src/components/Hangman.js
@@ -1,7 +1,7 @@
 import react, { useState, useEffect } from 'react';
 
 export default function Hangman({duration = 120000}) {
-    let word = "WUKONG";
+    let word = "Wukong";
     const alphabets = ["A", "B", "C", "D", "E", "F",
     "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
     "S", "T","U","V","W","X", "Y", "Z" ];
